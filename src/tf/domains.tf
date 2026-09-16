e24e46ae024a805dbb1cd59af3a6dc475adb2a06
Levi Zitting <me@levizitting.com>
2026-09-16 17:06:54 -0500

    feat: add DNS records for node 02 and update headscale

--- src/tf/domains.tf
+++ src/tf/domains.tf
@@ -24,12 +24,37 @@ resource "cloudflare_dns_record" "this_aaaa" {
   ttl     = 300
 }
 
+resource "cloudflare_dns_record" "node_02_a" {
+  zone_id = data.cloudflare_zone.levizitting_com.id
+  name    = local.node_02_hostname
+  type    = "A"
+  content = vultr_instance.node_02.main_ip
+  comment = local.dns_record_comment
+  proxied = false
+  ttl     = 300
+}
+
+resource "cloudflare_dns_record" "node_02_aaaa" {
+  zone_id = data.cloudflare_zone.levizitting_com.id
+  name    = local.node_02_hostname
+  type    = "AAAA"
+  content = cidrhost("${vultr_instance.node_02.v6_main_ip}/128", 0)
+  comment = local.dns_record_comment
+  proxied = false
+  ttl     = 300
+}
+
 resource "cloudflare_dns_record" "headscale_alias" {
   zone_id = data.cloudflare_zone.levizitting_com.id
   name    = local.headscale_hostname
   type    = "CNAME"
-  content = local.vps_fqdn
+  content = "${local.node_02_hostname}.${data.cloudflare_zone.levizitting_com.name}"
   comment = local.dns_record_comment
   proxied = false
   ttl     = 300
+
+  depends_on = [
+    cloudflare_dns_record.node_02_a,
+    cloudflare_dns_record.node_02_aaaa,
+  ]
 }
